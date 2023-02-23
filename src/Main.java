6cca228c465c1619beee5329f56502e4672f7047
Vahe <vabardeg@gmail.com>
2023-02-23 14:20:24 +0400

    i hope hw is done)

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Raphael", "'Van der Vaart'", 34));
+        people.add(new Person("Johan", "'Sebastian Bach'", 45));
+        people.add(new Person("Volfgang", "'Amadeus Mozart'", 36));
+        people.add(new Person("Jap", "'Van fon der Saar'", 25));
+        people.add(new Person("Karen", "'Ter-Balyants'", 34));
+        people.add(new Person("Ivan", "'Ivanov'", 28));
+
+        Collections.sort(people);
+        System.out.println("After sorting by default method\n" + people);
+
+        Collections.sort(people, new PersonsSurnameIncludeWordsComparator(3));
+        System.out.println("\nAfter sorting by my comparator method\n" + people);
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private String surname;
+    private int age;
+    private Set<String> surnameSet;
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.surnameSet = new HashSet<>(List.of(surname.split("\\P{IsAlphabetic}+")));
+    }
+    public int getSurnameSetLength() {
+        return surnameSet.size();
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + "-" + surname + "-" + age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (age < o.age) {
+            return -1;
+        } else if (age > o.age) {
+            return 1;
+        } else {
+            return name.compareTo(o.name);
+        }
+    }
+}

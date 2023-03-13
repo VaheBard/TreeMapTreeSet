1b9db89737381ebbede1a40019832bd0f4872f4f
Vahe <vabardeg@gmail.com>
2023-03-14 00:45:44 +0400

    second part of task

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
+
         List<Person> people = new ArrayList<>();
+
         people.add(new Person("Raphael", "'Van der Vaart'", 34));
         people.add(new Person("Johan", "'Sebastian Bach'", 45));
         people.add(new Person("Volfgang", "'Amadeus Mozart'", 36));
         people.add(new Person("Jap", "'Van fon der Saar'", 25));
         people.add(new Person("Karen", "'Ter-Balyants'", 34));
         people.add(new Person("Ivan", "'Ivanov'", 28));
+        people.add(new Person("Klim", "Omelyanovich", 10));
+        people.add(new Person("Emiliya", "Satanova", 6));
 
-        Collections.sort(people);
-        System.out.println("After sorting by default method\n" + people);
-
-        Collections.sort(people, new PersonsSurnameIncludeWordsComparator(4));
-        System.out.println("\nAfter sorting by my comparator method\n" + people);
+        System.out.println("List of all persons:\n" + people);
+        Predicate<Person> predicate = person -> person.getAge() < 18;
+        people.removeIf(predicate);
+        System.out.println("List persons over 18 years old:\n" + people);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -7,30 +7,38 @@ public class Person implements Comparable<Person> {
     private String surname;
     private int age;
     private final Set<String> surnameSet;
+
     public Person(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
         this.surnameSet = new HashSet<>(List.of(surname.split("\\P{IsAlphabetic}+")));
     }
+
     public int getSurnameSetLength() {
         return surnameSet.size();
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setSurname(String surname) {
         this.surname = surname;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public String getName() {
         return name;
     }
+
     public String getSurname() {
         return surname;
     }
+
     public int getAge() {
         return age;
     }
